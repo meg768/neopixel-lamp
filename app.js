da459f6827b4d721e28d289b78e7cffeea14c95d
meg768 <meg768@users.noreply.github.com>
2017-07-23 07:23:56 +0200

    -

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ var App = function() {
 			debug('Connected to socket server.');
 
 			// Register the service
-			socket.emit('service', {name:argv.room, timeout:1000});
+			socket.emit('service', {name:argv.room, timeout:10000});
 
 		});
 
