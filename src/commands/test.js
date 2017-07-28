4b09e210b138469c5b0e7efa0f09ef50f12b887d
meg768 <meg768@users.noreply.github.com>
2017-07-28 22:09:15 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -53,7 +53,7 @@ var Module = new function() {
 					console.log('Reply', data);
 					index = (index + 1) % 4;
 					colorIndex = (colorIndex + 1) % 3;
-					setTimeout(loop, data.error ? 5000 : 0);
+					setTimeout(loop, data.error ? 5000 : 200);
 				});
 
 			}
