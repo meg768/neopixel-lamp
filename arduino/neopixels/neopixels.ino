a5de15a80cc1149000ae2dbe6fb1aa84a6587158
meg768 <meg768@users.noreply.github.com>
2017-07-29 21:20:24 +0200

    -

--- arduino/neopixels/neopixels.ino
+++ arduino/neopixels/neopixels.ino
@@ -1,5 +1,6 @@
+#include <Wire.h>
+
 #include "neopixel-strip.h"
-#include "i2c.h"
 #include "blinker.h"
 
 class App;
@@ -8,7 +9,6 @@ class App;
 const int APP_I2C_ADDRESS  = 0x26;
 const int APP_NEOPIXEL_PIN = 4;
 const int APP_STRIP_LENGTH = 32;
-
 
 const int ACK = 6;
 const int NAK = 21;
@@ -24,15 +24,7 @@ const int ERR_PARAMETER_MISSING = 2;
 const int ERR_NOT_INITIALIZED   = 3;
 const int ERR_INVALID_COMMAND   = 4;
 
-
-const int LED_HEARTBEAT = 0;
-const int LED_ERROR     = 1;
-const int LED_BUSY      = 2;
-const int LED_DEBUG_1   = 3;
-const int LED_DEBUG_2   = 4;
-
 static App *_app = NULL;
-
 
 class App {
 
@@ -40,91 +32,141 @@ class App {
 
         App(int length, int pin) : _strip(length, pin) {
 
-            _app = this;
-            _status = ACK;
-            _loop = 0;
+            _app          = this;
+            _status       = ACK;
+            _loop         = 0;
+            _bufferIndex  = 0;
+            _bufferLength = 0;
+            
+            memset(_buffer, 0, sizeof(_buffer));
         }
 
-        void setup() {
-            io.onReceive(App::onReceiveService);
-            io.onRequest(App::onRequestService);
-            io.begin(APP_I2C_ADDRESS);
-            
-            _leds[LED_HEARTBEAT].setPin(13);
-            _leds[LED_ERROR].setPin(12);
-            _leds[LED_BUSY].setPin(11);
-            _leds[LED_DEBUG_1].setPin(10);
-            _leds[LED_DEBUG_2].setPin(9);
 
-            
-            for (int i = 0; i < 5; i++) {
-                _leds[i].blink();
-            }
-            
-            _strip.begin();
-            _strip.setColor(0, 0, 0);
-        }
-
-        static void onReceiveService() {
-            //_app->onReceive();
+        static void onReceiveService(int bytes) {
+            return _app->onReceive(bytes);
         }
 
         static void onRequestService() {
-            _app->onRequest();
+            return _app->onRequest();
+        }
+        
+        void setup() {
+            Wire.onReceive(App::onReceiveService);
+            Wire.onRequest(App::onRequestService);
+            Wire.begin(APP_I2C_ADDRESS);
+            
+            _hartBeat.setPin(13);
+            _error.setPin(12);
+            _busy.setPin(11);
+            _debug1.setPin(10);
+            _debug2.setPin(9);
+
+            
+            _strip.begin();
+            _strip.setColor(0, 0, 0);
+
+            _hartBeat.blink(5, 100);
+       
         }
 
-        void onReceive() {
-            _leds[LED_DEBUG_1].toggleState();
-            
-            if (io.available()) {
-                _leds[LED_BUSY].setState(HIGH);
-                _status = NAK;
 
-                int command = 0, error = ERR_INVALID_PARAMETER;
 
-                if (io.readByte(command))
-                    error = onCommand(command);
 
-                if (error != ERR_OK)
-                    _leds[LED_ERROR].toggleState();
+        void onReceive(int bytes) {
+            _debug1.setState(HIGH);
 
-                _status = ACK;
- 
-                _leds[LED_BUSY].setState(LOW);
+            // Clear buffer
+            memset(_buffer, 0, sizeof(_buffer));
 
-            }
+            if (bytes > sizeof(_buffer))
+                bytes = sizeof(_buffer);
 
+            // Read all bytes
+            for (int i = 0; i < bytes; i++)
+                _buffer[i] = Wire.read();
+
+            // Skip the rest if any
+            while (Wire.available())
+                Wire.read();
+
+            // Reset buffer
+            _bufferIndex  = 0;
+            _bufferLength = bytes;
+
+            _debug1.setState(LOW);
 
         }
 
         void onRequest() {
-            _leds[LED_DEBUG_2].toggleState();
-            io.write(_status);
+            _debug2.setState(HIGH);
+            Wire.write(_status);
+            _debug2.setState(LOW);
         }
+
+        int available() {
+            return _bufferLength - _bufferIndex;
+        }
+
+        int readByte(int &byte) {
+            if (!available())
+                return false;
+                
+            byte = _buffer[_bufferIndex++];
+
+            return true;
+        }
+       
+        int readRGB(int &red, int &green, int &blue) {
+            return readByte(red) && readByte(green) && readByte(blue);
+        }
+
+        int readWord(uint16_t &data) {
+            int high = 0, low = 0;
+
+            if (readByte(high) && readByte(low)) {
+                data = high << 8 | low;
+                return true;
+            }
+
+            return false;
+        };     
+
 
 
         void loop() {
-            io.idle();
-            
-            _loop++;
 
-            _leds[LED_HEARTBEAT].toggleState();
+            _hartBeat.toggleState();
 
-            onReceive();
-            delay(100);
+
+            if (available()) {
+                _busy.setState(HIGH);
+                _status = NAK;
+
+                if (parseRequest() != ERR_OK)
+                    _error.blink(2, 50);
+
+                _status = ACK; 
+                _busy.setState(LOW);
+
+                _bufferIndex  = 0;
+                _bufferLength = 0;
+            }
 
         };
 
 
-        int onCommand(int command) {
+        int parseRequest() {
 
-            static unsigned int foo = 0;
+            int command = 0;
+            
+            if (!readByte(command))
+                return ERR_INVALID_COMMAND;
 
             switch (command) {
                 case CMD_INITIALIZE: {
                     int length = 0;
 
-                    if (!io.readByte(length))
+                    if (!readByte(length))
                         return ERR_PARAMETER_MISSING;
 
                     if (length < 0 || length > 240)
@@ -142,12 +184,9 @@ class App {
 
                     int index = 0, lengthX = 0, red = 0, green = 0, blue = 0;
 
-                    if (!io.readByte(index) || !io.readByte(lengthX) || !io.readRGB(red, green, blue))
+                    if (!readByte(index) || !readByte(lengthX) || !readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
 
-
-//                    _strip.setColor(foo * 8, 8, red, green, blue);
-  //                  foo = (foo + 1) % 4;
                     _strip.setColor(index, lengthX, red, green, blue);
 
                     break;
@@ -157,10 +196,10 @@ class App {
 
                     int index = 0, length = 0, red = 0, green = 0, blue = 0, delay = 0;
 
-                    if (!io.readByte(index) || !io.readByte(length) || !io.readRGB(red, green, blue))
+                    if (!readByte(index) || !readByte(length) || !readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
 
-                    if (!io.readByte(delay))
+                    if (!readByte(delay))
                         return ERR_INVALID_PARAMETER;
 
                     _strip.wipeToColor(index, length, red, green, blue, delay);
@@ -173,10 +212,10 @@ class App {
                     int index = 0, length = 0, red = 0, green = 0, blue = 0;
                     uint16_t speed = 0;
                     
-                    if (!io.readByte(index) || !io.readByte(length) || !io.readRGB(red, green, blue))
+                    if (!readByte(index) || !readByte(length) || !readRGB(red, green, blue))
                         return ERR_INVALID_PARAMETER;
 
-                    if (!io.readWord(speed))
+                    if (!readWord(speed))
                         return ERR_INVALID_PARAMETER;
 
                     _strip.fadeToColor(index, length, red, green, blue, speed);
@@ -195,10 +234,12 @@ class App {
 
 
     private:
-        I2C io;
         NeopixelStrip _strip;
-        Blinker _leds[5];
+        Blinker _hartBeat, _error, _busy, _debug1, _debug2;
 
+        uint8_t _buffer[32]; 
+        int _bufferIndex, _bufferLength;
+    
         volatile int _status;
         volatile uint32_t _loop;
 
