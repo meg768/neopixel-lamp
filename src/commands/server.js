c3427c0d02264bc16eed5a0901a68073a62d70e9
meg768 <meg768@users.noreply.github.com>
2017-07-30 20:59:07 +0200

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -65,7 +65,7 @@ var Module = new function() {
 
 				var now = new Date();
 				var minutes = ((now.getHours() % 12) * 60) + now.getMinutes();
-				var hue = (minutes / 720);
+				var hue = 100 * (minutes / 720);
 
 				var options = {};
 				options.transition = 'fade';
