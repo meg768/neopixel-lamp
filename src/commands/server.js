6a5deef7b895137021a68cca704a54cc7185a164
meg768 <meg768@users.noreply.github.com>
2017-08-20 23:54:53 +0200

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -8,6 +8,71 @@ var config = require('../scripts/config.js');
 var io = require('socket.io-client');
 var SkyBightness = require('sky-brightness');
 
+/*
+
+class SkyBrightness {
+
+    constuctor(options) {
+
+        this.latitude = options.latitude;
+        this.longitude = options.longitude;
+        this.time = new Date();
+    }
+
+    // Returns an index of sun brightness 0 - 1, 1 - local zenith, 0 - local nadir
+    getSolarBrightness() {
+
+
+        var now = new Date();
+
+        var latitude = 55.7;
+        var longitude = 13.1833333;
+        var times = suncalc.getTimes(now, latitude, longitude);
+
+        var zenithPosition = suncalc.getPosition(times.solarNoon,  latitude, longitude);
+        var nadirPosition  = suncalc.getPosition(times.nadir,  latitude, longitude);
+        var thisPosition   = suncalc.getPosition(now, latitude, longitude);
+
+        return (thisPosition.altitude - nadirPosition.altitude) / (zenithPosition.altitude - nadirPosition.altitude);
+    }
+
+    getWeatherBrightness() {
+
+        return new Promise(function(resolve, reject) {
+            var request = new Request('https://query.yahooapis.com');
+
+            var query = {};
+            query.q      = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Lund, Sweden")'
+            query.format = 'json';
+            query.env    = 'store://datatables.org/alltableswithkeys';
+
+            request.get('/v1/public/yql', {query:query}).then(function(reply) {
+                console.log(reply.body.query.results.channel.item.condition);
+                resolve();
+            })
+            .catch(function(error) {
+                reject(error);
+            })
+
+        });
+
+    }
+
+    getLunarBrightness() {
+        return this.getSolarBrightness();
+
+    }
+
+    getPerceptualBrightness() {
+        return this.getSolarBrightness();
+    }
+
+
+
+}
+*/
+
+
 var Module = new function() {
 
 	function debug() {
@@ -60,7 +125,8 @@ var Module = new function() {
 				disableClock();
 				showClock();
 
-				timer = setInterval(showClock, 60000);
+				//timer = setInterval(showClock, 60000);
+                timer = setInterval(showClock, 1000);
 			}
 
 			function showClock() {
@@ -72,39 +138,29 @@ var Module = new function() {
 					time: new Date()
 				});
 
-/*
-				// Returns an index of sun brightness 0 - 1, 1 - local zenith, 0 - local nadir
-				function getSolarBrightness(now) {
-					var suncalc = require('suncalc');
+                sky.getPerceptualBrightness().then(function(brightness) {
+                    var now = new Date();
+    				var hue = (((now.getHours() % 12) * 60) + now.getMinutes()) / 2;
 
-					var latitude = 55.7;
-					var longitude = 13.1833333;
-					var times = suncalc.getTimes(now, latitude, longitude);
+    				var luminance = brightness * 100;;
 
-					var zenithPosition = suncalc.getPosition(times.solarNoon,  latitude, longitude);
-					var nadirPosition  = suncalc.getPosition(times.nadir,  latitude, longitude);
-					var thisPosition   = suncalc.getPosition(now, latitude, longitude);
+    				luminance = Math.min(100, luminance);
+    				luminance = Math.max(1, luminance);
 
-					return (thisPosition.altitude - nadirPosition.altitude) / (zenithPosition.altitude - nadirPosition.altitude);
-				}
-*/
+    				var options = {};
+    				options.transition = 'fade';
+    				options.duration   = 100;
+    				options.color      = {h:hue, s:100, l:luminance};
 
-				var now = new Date();
-				var hue = (((now.getHours() % 12) * 60) + now.getMinutes()) / 2;
+    				console.log(options);
 
-				var luminance = Math.round(sky.getPerceptualBrightness() * 100);
+    				strip.colorize(options);
 
-				luminance = Math.min(100, luminance);
-				luminance = Math.max(1, luminance);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
 
-				var options = {};
-				options.transition = 'fade';
-				options.duration   = 100;
-				options.color      = {h:hue, s:100, l:luminance};
-
-				console.log(options);
-
-				strip.colorize(options);
 			}
 
 
