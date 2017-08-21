bae28be018735ed66c226af4da0bdccb2a438057
meg768 <meg768@users.noreply.github.com>
2017-08-21 11:52:38 +0200

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -9,70 +9,6 @@ var io = require('socket.io-client');
 //var SkyBightness = require('sky-brightness');
 var SkyBightness = require('../scripts/sky-brightness.js');
 
-/*
-
-class SkyBrightness {
-
-    constuctor(options) {
-
-        this.latitude = options.latitude;
-        this.longitude = options.longitude;
-        this.time = new Date();
-    }
-
-    // Returns an index of sun brightness 0 - 1, 1 - local zenith, 0 - local nadir
-    getSolarBrightness() {
-
-
-        var now = new Date();
-
-        var latitude = 55.7;
-        var longitude = 13.1833333;
-        var times = suncalc.getTimes(now, latitude, longitude);
-
-        var zenithPosition = suncalc.getPosition(times.solarNoon,  latitude, longitude);
-        var nadirPosition  = suncalc.getPosition(times.nadir,  latitude, longitude);
-        var thisPosition   = suncalc.getPosition(now, latitude, longitude);
-
-        return (thisPosition.altitude - nadirPosition.altitude) / (zenithPosition.altitude - nadirPosition.altitude);
-    }
-
-    getWeatherBrightness() {
-
-        return new Promise(function(resolve, reject) {
-            var request = new Request('https://query.yahooapis.com');
-
-            var query = {};
-            query.q      = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Lund, Sweden")'
-            query.format = 'json';
-            query.env    = 'store://datatables.org/alltableswithkeys';
-
-            request.get('/v1/public/yql', {query:query}).then(function(reply) {
-                console.log(reply.body.query.results.channel.item.condition);
-                resolve();
-            })
-            .catch(function(error) {
-                reject(error);
-            })
-
-        });
-
-    }
-
-    getLunarBrightness() {
-        return this.getSolarBrightness();
-
-    }
-
-    getPerceptualBrightness() {
-        return this.getSolarBrightness();
-    }
-
-
-
-}
-*/
-
 
 var Module = new function() {
 
@@ -159,9 +95,11 @@ var Module = new function() {
 
                     })
                     .catch(function(error) {
+                        console.log('CANNOT GFET BRIGHTNESS');
                         console.log(error);
                     })
                     .then(function() {
+                        console.log('IHBORING ERROR');
                         resolve();
                     })
 
